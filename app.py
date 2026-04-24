8fb49afeffe8ecec4dc7264565f97184f843ce94
fjollal <120384284+fjollal@users.noreply.github.com>
2026-04-24 11:46:16 +0200

    First function

--- app.py
+++ app.py
@@ -2,3 +2,20 @@ import sqlite3
 
 #Lidhja e databazes
 db=sqlite3.connect('database.db')
+cursor=db.cursor()
+
+with open('schema.sql' ,'r') as f:
+    cursor.executescript(f.read())
+
+#First function create a mentee
+def create_mentee():
+    full_name = input("Enter your full name: ")
+    email = input("Enter your full email: ")
+    cohort = input("Enter your cohort: ")
+    cursor.execute(
+        'INSERT OR IGNORE INTO mentees(full_name,email,cohort) VALUES (?, ?, ?)',
+        (full_name, email, cohort)
+    )
+    db.commit()
+
+create_mentee()   
--- schema.sql
+++ schema.sql
@@ -1,8 +1,18 @@
 -- SQLite
 CREATE TABLE IF NOT EXISTS mentees(
-    id SERIAL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     cohort VARCHAR(50) NOT NULL,
     enrolled_date DATE DEFAULT CURRENT_DATE
 );
+
+INSERT OR IGNORE INTO mentees (full_name,email,cohort) VALUES
+('Fjolla Lushta', 'fjolla.example@gmail.com','B5'),
+('Blend Sejdiu', 'blend.sejdiu@gmail.com','B5'),
+('Dren Xhylyqi', 'dren.xhylyqi@gmail.com','B5'),
+('Ereza Greicevci', 'ereza.greicevci@gmail.com','B5'),
+('Vese Emini', 'vese.emini@gmail.com','B5'),
+('Lis Spahija', 'lis.spahija@gmail.com','B5');
+
+SELECT * FROM mentees;
